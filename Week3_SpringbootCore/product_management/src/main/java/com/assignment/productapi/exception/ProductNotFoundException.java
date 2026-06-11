e0a7984df24a1bf0dd53512abb681a29aee07cdc
Manali Sherkhane <manalisherkhane2000@gmail.com>
2026-06-11 23:49:44 +0530

    Remove accidental product_management folder

--- Week3_SpringbootCore/product_management/src/main/java/com/assignment/productapi/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.productapi.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
